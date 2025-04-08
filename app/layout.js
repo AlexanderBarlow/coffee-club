cf19fd006233df0150dd45aba1ee7c12d47eb144
Alexander Barlow <alex.barlow@rocketmail.com>
2025-04-07 20:10:21 -0400

    added customize screen

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta
 					name="apple-mobile-web-app-status-bar-style"
-					content="black-translucent"
+					content="translucent"
 				/>
 				<meta name="mobile-web-app-capable" content="yes" />
 				<meta name="theme-color" content="#6f4e37" />
